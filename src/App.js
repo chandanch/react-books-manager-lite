28e419a88aaca17d75bd078128c999e2d063dbbd
chandanch <chandan.cec107@gmail.com>
2025-02-28 12:33:54 +0530

    mutate state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with:", title);
+    books.push({ id: 222, title });
+    setBooks(books);
   };
 
   return (
