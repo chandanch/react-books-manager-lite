64e84f0f741d81068b28e5a550a5d6f033c1924e
chandanch <chandan.cec107@gmail.com>
2025-03-02 15:57:22 +0530

    add new book

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    books.push({ id: 222, title });
-    setBooks(books);
+    const updatedBooks = [...books, { id: 123, title }];
+    setBooks(updatedBooks);
   };
 
   return (
