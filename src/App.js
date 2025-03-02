e7da23212c5966ba358c2fdbd8798a278d074f1b
chandanch <chandan.cec107@gmail.com>
2025-03-02 15:59:54 +0530

    ✅ generate id

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { useState } from "react";
-import BookCreate from "./components/BookCreate";
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title }];
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
     setBooks(updatedBooks);
   };
 
   return (
     <div>
-      Books Count: {books.length}
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
